024f81f155408b2cb77048ae187b163714fafd52
Aryan Singh <arajsingh0505@gmail.com>
2026-09-07 02:38:55 +0530

    fix(backend): expand literal  in manage.py to prevent runserver CommandError

--- backend/manage.py
+++ backend/manage.py
@@ -4,5 +4,10 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qrapp.settings")
+
+    # Automatically expand literal $PORT or ${PORT} strings passed by platforms without shell expansion
+    port = os.environ.get("PORT", "8084")
+    sys.argv = [arg.replace("$PORT", port).replace("${PORT}", port) for arg in sys.argv]
+
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
